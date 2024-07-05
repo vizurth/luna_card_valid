8ceb5f4b75a9d26b3811395517388c836c0fea98
vizurth <timrsamara@gmail.com>
2024-07-05 19:26:13 +0400

    Initial Commit

---
+++ luna_card_valid/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  luna_card_valid
+//
+//  Created by Тимур Адиатуллин on 05.07.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
